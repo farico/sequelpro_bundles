9c9fd02457e423af5b65f0dff114a2727c6083b9
Aivaras Voveris <aivaras@voveris.eu>
2014-05-29 15:35:53 +0300

    Optimised for copying

--- copyAsArray.php
+++ copyAsArray.php
@@ -6,8 +6,12 @@ while($row = fgetcsv($stdIn, 0)) {
 	array_push($result, $row);
 }
 $columns = array_shift($result);
+if ($result) {
+	foreach($result as $k => $data) {
+		$result[$k] = array_combine($columns, $data);
+	}
+}
 ?>
-<h1><?php echo "Exported columns: " . implode(', ', $columns);?></h1>
 <pre>
 <?php var_export($result); ?>
 </pre>
